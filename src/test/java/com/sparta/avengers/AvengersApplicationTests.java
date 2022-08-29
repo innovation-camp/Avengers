c21aa497c1f5cd02991a841231307ebd17f81a1b
glooooow <2809yun@gmail.com>
2022-08-29 17:45:41 +0900

    created project

---
+++ src/test/java/com/sparta/avengers/AvengersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.avengers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvengersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
